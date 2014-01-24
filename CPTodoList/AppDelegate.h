3b4a388cf53910565bd76df7a846a6a2efd7b48e
Christine Wang <cwang@okta.com>
2014-01-24 10:38:48 -0800

    Initial Commit

---
+++ CPTodoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CPTodoList
+//
+//  Created by Christine Wang on 1/24/14.
+//  Copyright (c) 2014 Christine Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CPTodoListTests/CPTodoListTests.m
@@ -0,0 +1,34 @@
+//
+//  CPTodoListTests.m
+//  CPTodoListTests
+//
+//  Created by Christine Wang on 1/24/14.
+//  Copyright (c) 2014 Christine Wang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CPTodoListTests : XCTestCase
+
+@end
+
+@implementation CPTodoListTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
